97fdaffdd85bb7dc86df931e4af65816d82df258
Muralidhargithub <ot41975@umbc.edu>
2024-12-12 11:59:03 -0500

    NetworkUpdated

--- TabBarApiApp/NetworkError/NetworkError.swift
+++ TabBarApiApp/NetworkError/NetworkError.swift
@@ -14,5 +14,4 @@ enum NetworkError: Error {
     case invalidResponse
     case invalidData
     case invalidJSON
-
 }
--- TabBarApiApp/NetworkManager/NetworkManagerAPi.swift
+++ TabBarApiApp/NetworkManager/NetworkManagerAPi.swift
@@ -9,39 +9,47 @@
 import Foundation
 import UIKit
 
+// Protocol for fetching data and images
 protocol ImageDef {
-    func getData<T:Codable>(url:String) async throws -> T
+    func getData<T: Codable>(url: String) async throws -> T
     func getImage(url: String) async throws -> UIImage
 }
 
-class ImageNetwork: ImageDef{
+class ImageNetwork: ImageDef {
     // MARK: Properties
     
     static let sharedInstance = ImageNetwork()
-    let urlSession: URLSession
-    var imageCache = NSCache<NSString, UIImage>()
+    private let urlSession: URLSession
+    private var imageCache = NSCache<NSString, UIImage>()
     
-    //MARK: INitialization
+    // MARK: Initialization
     private init() {
         let config = URLSessionConfiguration.default
         self.urlSession = URLSession(configuration: config)
     }
     
-    //MARK: Function to fetch data
-    func getData<T:Codable>(url:String) async throws -> T {
+    // MARK: Function to fetch data
+    func getData<T: Codable>(url: String) async throws -> T {
         guard let serverURL = URL(string: url) else {
             throw NetworkError.invalidURL
         }
         
-        let (data, _) = try await urlSession.data(from: serverURL)
+        let (data, response) = try await urlSession.data(from: serverURL)
+        
+        // Check for valid response 
+        guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
+            throw NetworkError.invalidResponse
+        }
+        
+        // Decode JSON data
         let decoded = try JSONDecoder().decode(T.self, from: data)
         return decoded
-        
     }
-                            
-    //MARK: Function to fetchImages
+    
+    // MARK: Function to fetch Images with caching
     func getImage(url: String) async throws -> UIImage {
-        if let cachedImage = imageCache.object(forKey: url as NSString){
+        // Check if the image is already cached
+        if let cachedImage = imageCache.object(forKey: url as NSString) {
             return cachedImage
         }
         
@@ -49,11 +57,21 @@ class ImageNetwork: ImageDef{
             throw NetworkError.invalidURL
         }
         
-        let (data, _) = try await urlSession.data(from: serverURL)
+        // Fetch the image data
+        let (data, response) = try await urlSession.data(from: serverURL)
+        
+        // Check for valid response
+        guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
+            throw NetworkError.invalidResponse
+        }
+        
         guard let image = UIImage(data: data) else {
             throw NetworkError.invvalidImageData
         }
+        
+        // Cache the image for future use
         imageCache.setObject(image, forKey: serverURL.absoluteString as NSString)
+        
         return image
     }
 }
