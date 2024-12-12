e0bb1745771a78215d4a83ccaf9e24d14ab01a1d
Muralidhargithub <ot41975@umbc.edu>
2024-12-12 12:14:42 -0500

    Updated Tab Bar Controller using Program

--- TabBarApiApp/SceneDelegate.swift
+++ TabBarApiApp/SceneDelegate.swift
@@ -13,11 +13,32 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
-    }
+            guard let windowScene = (scene as? UIWindowScene) else { return }
+
+            // Create a window
+            window = UIWindow(windowScene: windowScene)
+
+            // Create the tab bar controller
+            let tabBarController = UITabBarController()
+
+            // Create view controllers
+            let PhotoVC = FirstVC()
+            let SubscriberVC = thirdVC()
+            let ArticleVC = ArticleListVC()
+            let articleNavController = UINavigationController(rootViewController: ArticleVC)
+            // Set tab bar items
+            PhotoVC.tabBarItem = UITabBarItem(title: "Photos", image: UIImage(systemName: "star"), tag: 0)
+            SubscriberVC.tabBarItem = UITabBarItem(title: "Subscribers", image: UIImage(systemName: "star.fill"), tag: 1)
+            articleNavController.tabBarItem = UITabBarItem(title: "Articles", image: UIImage(systemName: "star"), tag: 2)
+
+
+            // Set the view controllers for the tab bar controller
+            tabBarController.viewControllers = [articleNavController, SubscriberVC,PhotoVC]
+
+            // Set the window's root view controller
+            window?.rootViewController = tabBarController
+            window?.makeKeyAndVisible()
+        }
 
     func sceneDidDisconnect(_ scene: UIScene) {
         // Called as the scene is being released by the system.
